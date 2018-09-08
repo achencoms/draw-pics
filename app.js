782e7d068a732fd015c9ed04aaf4e9be86c31d5e
achencoms <achencoms@gmail.com>
2018-09-08 11:17:51 -0400

    Working game functionality implemented
    Able to submit answers, name tags appear as they should, and the timer is implemented as well.

--- app.js
+++ app.js
@@ -7,23 +7,38 @@ const io = require("socket.io")(http);
 //Having a dynamic port to allow for Heroku or another hosting service to choose custom port
 const port = process.env.PORT || 3000;
 
-//Keeps track of the current drawing and current state of the game
+//Colors for the name tags
+const colors = ["red","black","blue","purple","green","pink","lime","orange"];
+
+//Keeps track of the current drawing
 let currDrawing = [];
 let currStep = -1;
+
+//Keeps track of the players in the game by name
+let nameMappings = {};
+
+//Keeps track of the letters currently shown and the chosen word and user
 let currLetters = [];
 let chosenWord = "";
 let chosenUser = "";
-let globalTimer, timer;
+
+//Keeps track of the state of the game
+let globalTimer, letterTimer;
+let time, correctResponses;
 let notStarted = true;
 
+//Reading in and only choosing words with length greater than 3
 const words = fs.readFileSync("words.txt","utf-8").split("\n").filter(word => word.length > 3);
 
+//Allows use of static files in public directory
 app.use(express.static("public"));
 
+//Upon entering, the users will be shown the name input
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+//Establising a Socket.IO connection with the client
 io.on("connection", function(socket) {
   console.log("a user has connected!");
 
@@ -33,14 +48,41 @@ io.on("connection", function(socket) {
   if(notStarted){
     setup();
   }
+  
+  //Receiving the users name and attaching it to the socket
+  socket.on("name", function(data){
+    const id = socket.id;
+    nameMappings[id] = data;
+    if(notStarted) setup();
+  });
 
   //Receiving user's answers and directing them to all users
   socket.on("answer", function(data){
-    if(data === chosenWord){
-      clearInterval(globalTimer);
-      setup();
+    const msg = data.trim();
+    if(msg != "" && msg === chosenWord){
+      //Upon answering, the tracker for the number of people who got it right will increase
+      correctResponses++;
+      const numOfGuessers = activePlayers() - 1;
+      if(correctResponses === numOfGuessers){
+        io.emit("message", {"msg" : `Everyone got the correct answer! The next round will be starting soon...`});
+        clearTimers();
+        io.emit("time",time);
+        setTimeout(function(){
+          setup();
+        }, 5000);
+      }
+      //Upon answering, the time will shorten to start the next round faster
+      else {
+        if(time > 10) time = 10;
+        io.emit("message", {"msg" : `${nameMappings[socket.id]} got the correct answer!`});
+      }
+    } else{
+      //Redirect the current answer to the general chat
+      io.emit("message", {
+        "msg" : msg,
+        "name" : nameMappings[socket.id]
+      });
     }
-    io.emit("message", data);
   });
 
   //Drawing data is broadcast to all connected sockets
@@ -90,7 +132,20 @@ io.on("connection", function(socket) {
 
   //Resets the game to allow for the new connected sockets to join
   socket.on("reset", function(){
+    clearTimers();
     setup();
+  });
+
+  //Detect whether or not the user has disconnected
+  socket.on("disconnect", function(){
+    delete nameMappings[socket.id];
+    console.log("A user has disconnected!");
+    //Ending the game if there is less than 2 users to play.
+    if(activePlayers() < 2){
+      clearTimers();
+      io.emit("reset");
+      notStarted = true;
+    }
   });
 
 });
@@ -100,53 +155,89 @@ http.listen(port, function() {
 });
 
 //Helper Functions
+//Chooses a random item from the provided array, specifically current letters or avaliable clients
 function randChoice(array){
-  let randChoice = array[Math.floor(Math.random() * array.length)];
+  let randChoice = array[randIndex(array.length)];
   if(array === words){
     currLetters = [];
     while(randChoice === chosenWord){
-      randChoice = array[Math.floor(Math.random() * array.length)];
+      randChoice = array[randIndex(array.length)];
     }
     chosenWord = randChoice.trim();
-    currLetters = Array(chosenWord.length).fill("");
+    for(let i = 0; i < chosenWord.length; i++){
+      if(chosenWord.charAt(i) == " "){
+        currLetters.push("-");
+      } else currLetters.push("");
+    }
   } else{
     while(randChoice === chosenUser){
-      randChoice = array[Math.floor(Math.random() * array.length)];
+      randChoice = array[randIndex(array.length)];
     }
     chosenUser = randChoice;
   }
   return randChoice;
 }
 
+//Chooses a random letter from the chosen word
 function randLetter(){
-  let randIndex = Math.floor(Math.random() * chosenWord.length);
-  while(currLetters[randIndex] != ""){
-    randIndex = Math.floor(Math.random() * chosenWord.length);
+  let index = randIndex(chosenWord.length);
+  //Reassigns index if the current letter has already been used
+  while(currLetters[index] != ""){
+    index = randIndex(chosenWord.length);
   }
-  currLetters[randIndex] = chosenWord.charAt(randIndex);
+  //Check 
+  currLetters[index] = chosenWord.charAt(index);
 }
 
 function setup(){
   io.sockets.clients((error,clients) => {
      //Checking if the game has enough users to begin the game
-    if(clients.length > 1){
+    if(activePlayers() > 1){
+      //Once the game has been initiated, it will not allow new players to join in
       notStarted = false;
+      
+      //Clearing the users' screens and choosing the drawer
+      time = 70;
+      correctResponses = 0;
       io.emit("reset");
+      io.emit("time",time);
       io.to(randChoice(clients)).emit("chosen",randChoice(words));
       io.emit("word",currLetters);
       
       //Setup the timer to give letters until there are only 2 missing
-      timer = setInterval(function(){
+      letterTimer = setInterval(function(){
         randLetter();
         io.emit("word",currLetters);
-      }, 60000 / (chosenWord.length - 2));
+      }, 60000 / (chosenWord.length / 2));
   
       //Setup global timer for the game
+      //Maybe implement it so that there is a cooldown period where the game announces that the next round is about to start
       globalTimer = setInterval(function(){
-        clearInterval(timer);
-        clearInterval(globalTimer);
-        setup();
-      },60000 + 10000 / (chosenWord.length));
+        if(time === 1){
+          clearTimers();
+          io.emit("time",time);
+          io.emit("message", {"msg" : "The next round will be starting soon..."}); 
+          setTimeout(function(){
+            setup();
+          }, 5000);
+        }
+        time--;
+        io.emit("time",time);
+      }, 1000);
     }
   });
+}
+
+function randIndex(max){
+  return Math.floor(Math.random() * max);
+}
+
+function clearTimers(){
+  clearInterval(letterTimer);
+  clearInterval(globalTimer);
+  time = 0;
+}
+
+function activePlayers(){
+  return Object.keys(nameMappings).length;
 }
---
+++ index.js
@@ -0,0 +1,237 @@
+$(document).ready(function() {
+  const socket = io();
+  const curr = {
+    tool: "pencil",
+    isChosen: false,
+    color: "black"
+  };
+
+  //Listens for any new drawing data
+  socket.on("draw", function(data) {
+    drawLine(data);
+  });
+
+  //removes all the previous and existing states of the canvas in the server
+  socket.on("clear", function() {
+    clearCanvas();
+  });
+
+  //Listens for any new erased data
+  socket.on("eraser", function(data) {
+    erase(data);
+  });
+
+  //Listens for any calls to redraw the canvas, used to redo and undo
+  socket.on("redraw", function(data) {
+    redraw(data);
+  });
+
+  //Resets the ui to reflect a user answering
+  socket.on("reset", function(){
+    socket.emit("clear");
+    $(".timer").fadeOut();
+    $(".chat").empty();
+    $("#word").empty();
+    $(".chat-group").hide();
+    $(".drawing-tools").hide();
+    $("canvas").css("cursor","default");
+    curr.isChosen = false;
+  });
+
+  //Used to notify the user that he or she has been chosen to draw
+  socket.on("chosen", function(data){
+    $(".chat-group").hide();
+    $(".wordReveal").text(`Current Word: ${data}`);
+    $(".drawing-tools").css("display","flex");
+    $("canvas").css("cursor","url(pencil.png), auto");
+    curr.isChosen = true;
+  });
+
+  //Players will receive the current state of the word to display on the screen
+  socket.on("word",function(data){
+    $(".timer").fadeIn();
+    if(!curr.isChosen){
+      $(".chat-group").show();
+      setupWord(data);
+    }
+  });
+
+  //Receiving the current round time
+  socket.on("time", function(data){
+    if(data === 0 ) $(".timer").fadeOut();
+    $(".timer").text(data);
+  });
+
+  //Disable the ability to answer any further
+  socket.on("correct", function(data){
+    $(".chat-group").hide();
+  });
+
+  //Receives the emitted message from the server
+  socket.on("message", function(data){
+    let output = `<p><b>${data.msg}</b></p>`;
+    if(data.name){
+      output = `<p><b style='background-color:black; border-radius: 10%; color: white'>${data.name}</b>: ${data.msg}</p>`;
+    }
+    $(".chat").append(output);
+    $(".chat").animate({
+      scrollTop: $(document).height()
+    }, 400)
+  });
+
+  //keeping track of canvas and whether or not the user is currently drawing
+  const c = document.getElementById("main");
+  const ctx = c.getContext("2d");
+  let isMouseDown = false;
+
+  function getMousePos(canvas, evt) {
+    const rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+  }
+
+  //Draws based position parameters and pencil confirguration
+  function drawLine({ x1, y1, x2, y2, width, color}) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width;
+    ctx.stroke();
+  }
+
+  //Forces the canvas to draw over itself with an image (memory intensive compared to redrawing a saved state of instruction set, but much smoother)
+  function redraw(data) {
+    let lastDraw = new Image();
+    lastDraw.src = data;
+    lastDraw.onload = function() {
+      clearCanvas();
+      ctx.drawImage(lastDraw, 0, 0);
+    };
+  }
+
+  //Erases the canvas at the position of the cursor
+  function erase(data) {
+    ctx.clearRect(data.x + 13, data.y + 4, 20, 39);
+  }
+
+  //Clears the canvas
+  function clearCanvas() {
+    ctx.clearRect(0, 0, c.width, c.height);
+  }
+
+  //Setup for the game, specifically the word
+  function setupWord(letters){
+    $("#word").empty();
+    for(let i = 0 ; i < letters.length; i++){
+      let newEmpty = $("<input>",{"type": "text", "class" : "letter", "value" : letters[i], "disabled" : "disabled", "maxLength" : "1"});
+      $("#word").append(newEmpty);
+    }    
+  }
+
+  //Allows the user to edit the canvas depending on whether or not the mouse button is down
+  c.addEventListener("mousedown", function(evt) {
+    if(curr.isChosen){
+      isMouseDown = true;
+      const pos = getMousePos(c, evt);
+      curr.x = pos.x;
+      curr.y = pos.y;
+      if (curr.tool == "eraser") {
+        ctx.clearRect(pos.x + 13, pos.y + 4, 20, 39);
+      }
+    }
+  });
+
+  //Upon release of the mouse button, the current state of the canvas is saved for undo/redo.
+  c.addEventListener("mouseup", function(evt) {
+    if(curr.isChosen){
+      isMouseDown = false;
+      currentDrawing = c.toDataURL();
+      socket.emit("saveDrawing", currentDrawing);
+    }
+  });
+
+  //Gathers positional information about the cursor
+  c.addEventListener("mousemove", function(evt) {
+    if(curr.isChosen && isMouseDown){
+        const pos = getMousePos(c, evt);
+        if (curr.tool == "pencil") {
+          const x = pos.x;
+          const y = pos.y;
+          const data = {
+            x1: curr.x,
+            y1: curr.y,
+            x2: x,
+            y2: y,
+            width: $("#radius").val(),
+            color: curr.color
+          };
+          socket.emit("drawData", data);
+          curr.x = x;
+          curr.y = y;
+        } else {
+          socket.emit("eraser", pos);
+        }
+    }
+    
+  });
+
+  //Submits the name that the user chooses
+  $("#name-sub").on("click",function(evt){
+    evt.preventDefault();
+    const name = $("#name-box").val();
+    //Check if the name isn't empty
+    if(name.trim() != ""){
+       //Save the name for later use in server to keep track of points
+      socket.emit("name",name);
+      $(".name").hide();
+      $(".game").show();
+    }
+  });
+
+  //Submits the current value in the textbox and clears it
+  $("#answer-sub").on("click",function(evt){
+    evt.preventDefault();
+    const ans = $("#answer");
+    socket.emit("answer",ans.val());
+    ans.val("");
+  });
+
+  //Calls upon clearCanvas() and clears the entirety of the save states
+  $("#clear").on("click", function() {
+    socket.emit("clear");
+  });
+
+  //Either reverts to a previous state of the canvas or a more current state of the canvas (undo/redo)
+  $(".do").on("click", function() {
+    const id = $(this).attr("id");
+    socket.emit(id);
+  });
+
+  //Allows the drawer to reset the game so that he or she cans skip turn
+  $(".reset").on("click", function(){
+    socket.emit("reset");
+  });
+
+  //Chooses color based on which color block is clicked
+  $(".colors-item").on("click",function(){
+    const color = $(this).css("background-color");
+    curr.color = color;
+  });
+
+  //Configures which tool the user wishes to use
+  $(".option").on("click", function() {
+    $(".option").removeClass("selected");
+    $(this).addClass("selected");
+    const id = $(this).attr("id");
+    if (id === "pencil") {
+      curr.tool = "pencil";
+      $("canvas").css("cursor", "url(pencil.png), auto");
+    } else if (id === "eraser") {
+      curr.tool = "eraser";
+      $("canvas").css("cursor", "url(eraser.png), auto");
+    }
+  });
+});
