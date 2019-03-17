31ac09cea1bc371b374b2df220cbd6ee7ac168a5
Anthony <achencoms@gmail.com>
2019-03-17 12:05:20 -0400

    Simple design changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require("fs");
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
@@ -8,7 +8,7 @@ const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
 //Colors for the name tags
-const colors = ["red","black","blue","purple","green","pink","lime","orange"];
+const colors = ["red", "blue", "purple", "green", "pink", "lime", "orange"];
 
 //Keeps track of the current drawing
 let currDrawing = [];
@@ -16,6 +16,7 @@ let currStep = -1;
 
 //Keeps track of the players in the game by name
 let nameMappings = {};
+let colorMappings = {};
 
 //Keeps track of the letters currently shown and the chosen word and user
 let currLetters = [];
@@ -28,7 +29,10 @@ let time, correctResponses;
 let notStarted = true;
 
 //Reading in and only choosing words with length greater than 3
-const words = fs.readFileSync("words.txt","utf-8").split("\n").filter(word => word.length > 3);
+const words = fs
+  .readFileSync("words.txt", "utf-8")
+  .split("\n")
+  .filter(word => word.length > 3);
 
 //Allows use of static files in public directory
 app.use(express.static("public"));
@@ -43,44 +47,54 @@ io.on("connection", function(socket) {
   console.log("a user has connected!");
 
   //Upon connecting, a new socket will be able to see what was already drawn
-  if(currDrawing.length) socket.emit("redraw", currDrawing[currStep]);
+  if (currDrawing.length) socket.emit("redraw", currDrawing[currStep]);
 
-  if(notStarted){
+  if (notStarted) {
     setup();
   }
-  
+
   //Receiving the users name and attaching it to the socket
-  socket.on("name", function(data){
+  socket.on("name", function(data) {
     const id = socket.id;
     nameMappings[id] = data;
-    if(notStarted) setup();
+    colorMappings[id] = randChoice(colors);
+    if (notStarted) setup();
+    else socket.emit("word", currLetters);
   });
 
   //Receiving user's answers and directing them to all users
-  socket.on("answer", function(data){
+  socket.on("answer", function(data) {
     const msg = data.trim();
-    if(msg != "" && msg === chosenWord){
+    if (msg == "") return;
+    if (msg != "" && msg === chosenWord) {
       //Upon answering, the tracker for the number of people who got it right will increase
       correctResponses++;
       const numOfGuessers = activePlayers() - 1;
-      if(correctResponses === numOfGuessers){
-        io.emit("message", {"msg" : `Everyone got the correct answer! The next round will be starting soon...`});
+      if (correctResponses === numOfGuessers) {
+        io.emit("message", {
+          msg: `Everyone got the correct answer! The next round will be starting soon...`
+        });
+        io.emit("showWord", chosenWord);
         clearTimers();
-        io.emit("time",time);
-        setTimeout(function(){
+        io.emit("time", 0);
+        setTimeout(function() {
           setup();
         }, 5000);
       }
       //Upon answering, the time will shorten to start the next round faster
       else {
-        if(time > 10) time = 10;
-        io.emit("message", {"msg" : `${nameMappings[socket.id]} got the correct answer!`});
+        if (time > 15) time = 15;
+        io.emit("time", 15);
+        io.emit("message", {
+          msg: `${nameMappings[socket.id]} got the correct answer!`
+        });
       }
-    } else{
+    } else {
       //Redirect the current answer to the general chat
       io.emit("message", {
-        "msg" : msg,
-        "name" : nameMappings[socket.id]
+        msg: msg,
+        name: nameMappings[socket.id],
+        color: colorMappings[socket.id]
       });
     }
   });
@@ -131,23 +145,22 @@ io.on("connection", function(socket) {
   });
 
   //Resets the game to allow for the new connected sockets to join
-  socket.on("reset", function(){
+  socket.on("reset", function() {
     clearTimers();
     setup();
   });
 
   //Detect whether or not the user has disconnected
-  socket.on("disconnect", function(){
+  socket.on("disconnect", function() {
     delete nameMappings[socket.id];
     console.log("A user has disconnected!");
-    //Ending the game if there is less than 2 users to play.
-    if(activePlayers() < 2){
+    //Ending the game if there are less than 2 users to play.
+    if (activePlayers() < 2) {
       clearTimers();
       io.emit("reset");
       notStarted = true;
     }
   });
-
 });
 
 http.listen(port, function() {
@@ -155,22 +168,23 @@ http.listen(port, function() {
 });
 
 //Helper Functions
-//Chooses a random item from the provided array, specifically current letters or avaliable clients
-function randChoice(array){
-  let randChoice = array[randIndex(array.length)];
-  if(array === words){
+//Chooses a random item from the provided array, specifically current letters or avaliable clients or colors
+function randChoice(array) {
+  let rIndex = randIndex(array.length);
+  let randChoice = array[rIndex];
+  if (array === words) {
     currLetters = [];
-    while(randChoice === chosenWord){
+    while (randChoice === chosenWord) {
       randChoice = array[randIndex(array.length)];
     }
     chosenWord = randChoice.trim();
-    for(let i = 0; i < chosenWord.length; i++){
-      if(chosenWord.charAt(i) == " "){
+    for (let i = 0; i < chosenWord.length; i++) {
+      if (chosenWord.charAt(i) == " ") {
         currLetters.push("-");
       } else currLetters.push("");
     }
-  } else{
-    while(randChoice === chosenUser){
+  } else {
+    while (randChoice === chosenUser) {
       randChoice = array[randIndex(array.length)];
     }
     chosenUser = randChoice;
@@ -179,65 +193,67 @@ function randChoice(array){
 }
 
 //Chooses a random letter from the chosen word
-function randLetter(){
+function randLetter() {
   let index = randIndex(chosenWord.length);
   //Reassigns index if the current letter has already been used
-  while(currLetters[index] != ""){
+  while (currLetters[index] != "") {
     index = randIndex(chosenWord.length);
   }
-  //Check 
+  //Check
   currLetters[index] = chosenWord.charAt(index);
 }
 
-function setup(){
-  io.sockets.clients((error,clients) => {
-     //Checking if the game has enough users to begin the game
-    if(activePlayers() > 1){
+function setup() {
+  io.sockets.clients((error, clients) => {
+    //Checking if the game has enough users to begin the game
+    if (activePlayers() > 1) {
       //Once the game has been initiated, it will not allow new players to join in
       notStarted = false;
-      
+
       //Clearing the users' screens and choosing the drawer
       time = 70;
       correctResponses = 0;
       io.emit("reset");
-      io.emit("time",time);
-      io.to(randChoice(clients)).emit("chosen",randChoice(words));
-      io.emit("word",currLetters);
-      
+      io.emit("time", time);
+      io.to(randChoice(clients)).emit("chosen", randChoice(words));
+      io.emit("word", currLetters);
+
       //Setup the timer to give letters until there are only 2 missing
-      letterTimer = setInterval(function(){
+      letterTimer = setInterval(function() {
         randLetter();
-        io.emit("word",currLetters);
+        io.emit("word", currLetters);
       }, 60000 / (chosenWord.length / 2));
-  
+
       //Setup global timer for the game
       //Maybe implement it so that there is a cooldown period where the game announces that the next round is about to start
-      globalTimer = setInterval(function(){
-        if(time === 1){
+      globalTimer = setInterval(function() {
+        if (time === 1) {
           clearTimers();
-          io.emit("time",time);
-          io.emit("message", {"msg" : "The next round will be starting soon..."}); 
-          setTimeout(function(){
+          io.emit("time", time);
+          io.emit("showWord", chosenWord);
+          io.emit("message", {
+            msg: "The next round will be starting soon..."
+          });
+          setTimeout(function() {
             setup();
           }, 5000);
         }
         time--;
-        io.emit("time",time);
+        io.emit("time", time);
       }, 1000);
     }
   });
 }
 
-function randIndex(max){
+function randIndex(max) {
   return Math.floor(Math.random() * max);
 }
 
-function clearTimers(){
+function clearTimers() {
   clearInterval(letterTimer);
   clearInterval(globalTimer);
-  time = 0;
 }
 
-function activePlayers(){
+function activePlayers() {
   return Object.keys(nameMappings).length;
 }
